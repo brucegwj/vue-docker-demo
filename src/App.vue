610a90053f929356d50bdb74197c3b564a0ce9bc
gaowj <gaowj@datacloudchina.com>
2018-03-01 11:32:35 +0800

    vue-docker

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <top-gitment :options="options" v-if="options"></top-gitment> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      options: {
+        id: 'article id',
+        owner: 'Your GitHub ID',
+        repo: 'The repo to store comments',
+        oauth: {
+          client_id: 'Your client ID',
+          client_secret: 'Your client secret'
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
